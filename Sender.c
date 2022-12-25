63558d15432c14946b0b2d08c491aadff101b7b7
Yuval <yuvit256@gmail.com>
2022-12-26 01:29:42 +0200

    updated sender message

--- Sender.c
+++ Sender.c
@@ -20,14 +20,19 @@ int main()
     FILE *file = fopen("f.txt", "r");
     if(file == NULL)
     {
+        printf("Couldn't open the file correctly");
         return 0;
     }
-    fclose(file);
-
+    if( fclose(file) == NULL)
+    {
+        printf("Couldn't close the file correctly");
+        return 0;
+    }
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock <= 0)
+    if(sock == -1)
     {
+        printf("Couldn't create the socket correctly");
         return 0;
     }
     
@@ -37,16 +42,17 @@ int main()
     server_address.sin_port = htons(PORT);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    if(connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0)
+    if(connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) == -1)
     {
+        printf("Couldn't create the connection correctly");
         return 0;
     }
     printf("connected!");
 
     char *message = "Its Maor Berenstein!";
-    if(send(sock, message, strlen(message) + 1, 0) <= 0)
+    if(send(sock, message, strlen(message) + 1, 0) == -1)
     {
-        printf("err");
+        printf("Couldn't send the message correctly");
         return 0;
     }
 
